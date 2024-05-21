8aa5ae3fceaeb16b2c4f1a7b2a851cac080edda1
refokcer <refokcer@gmail.com>
2024-05-21 22:09:25 +0300

    Commit create manager

--- online_shop/wsgi.py
+++ online_shop/wsgi.py
@@ -2,11 +2,11 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-from accounts.views import create_manager
+# from accounts.views import create_manager
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'online_shop.settings')
 
 # create user with 'manager' role
-create_manager()
+# create_manager()
 
 application = get_wsgi_application()

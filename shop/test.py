5a19d663783977acd9ea27c3065806e1e734cd69
refokcer <refokcer@gmail.com>
2024-05-22 20:24:41 +0300

    test views shop

--- shop/test.py
+++ shop/test.py
@@ -8,14 +8,14 @@ from shop.models import Product, Category
 
 
 class ShopTests(TestCase):
-    # def setUp(self):
-    #     self.user = User.objects.create_user(email='testuser@gmail.com', full_name="test", password='12345')
-    #     self.order = Order.objects.create(user=self.user)
-    #     self.category = Category.objects.create(title='test', slug='test')
-    #     self.product = Product.objects.create(category=self.category, image='test.jpg', price=10,
-    #                                           title="test", description="test", slug="test")
-    #     self.orderItem = OrderItem.objects.create(order=self.order, product=self.product, quantity=3, price=10)
-    #     self.orderItem2 = OrderItem.objects.create(order=self.order, product=self.product, quantity=2, price=121)
+    def setUp(self):
+        self.user = User.objects.create_user(email='testuser@gmail.com', full_name="test", password='12345')
+        self.order = Order.objects.create(user=self.user)
+        self.category = Category.objects.create(title='test', slug='test')
+        self.product = Product.objects.create(category=self.category, image='test.jpg', price=10,
+                                              title="test", description="test", slug="test")
+        self.orderItem = OrderItem.objects.create(order=self.order, product=self.product, quantity=3, price=10)
+        self.orderItem2 = OrderItem.objects.create(order=self.order, product=self.product, quantity=2, price=121)
 
     def test_user_shop_view_favorites(self):
         self.client.login(email='testuser@gmail.com', password='12345')
